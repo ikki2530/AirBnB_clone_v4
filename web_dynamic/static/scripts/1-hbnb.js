a6e20f2f9966fd7a89beba526031d718f68e5b58
Diego Gomez <dagomez2530@gmail.com>
2020-09-25 22:44:20 +0000

    Adding js file

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,14 @@
-$(document).ready(function (){
+$(document).change(function (){
   ids_amenities = [];
-
-  $.each("$input[data-id=':amenity_id']:checked", function () {
-    ids_amenities.push($(this).val())
+  name_amenities = [];
+  $.each($("input[type='checkbox']:checked"), function () {
+    ids_amenities.push($(this).attr('data-id'));
+    name_amenities.push($(this).attr('data-name'));
   });
+  console.log(ids_amenities, name_amenities);
+  let txt = '';
+  for (let i = 0; i < name_amenities.length; i++) {
+    txt += name_amenities[i] + ", ";
+  }
+  $('.amenities h4').text(txt);
 });
