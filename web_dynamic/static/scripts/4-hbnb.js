5878f9517bfed7f21bf9f1cb6165e54e124bb33c
Diego Gomez <dagomez2530@gmail.com>
2020-09-26 21:29:57 +0000

    Fixing errors

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -7,6 +7,7 @@ $(document).change(function (){
   name_amenities = [];
   $.each($("input[type='checkbox']:checked"), function () {
     ids_amenities.push($(this).attr('data-id'));
+    console.log($(this).attr('data-id'), $(this).attr('data-name'))
     name_amenities.push($(this).attr('data-name'));
   });
   ids_amenities_copy = ids_amenities;
@@ -17,7 +18,6 @@ $(document).change(function (){
     txt += name_amenities[i] + ", ";
   }
   $('.amenities h4').text(txt);
-  console.log(txt)
 });
 // 3 check if api status is ok.
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
@@ -27,46 +27,20 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
   else {
     $("#api_status").removeClass("available");
   }
-})
+});
 
-
-// // 4 send a post request to get a list of places
-// $.ajax({
-//   url: 'http://0.0.0.0:5001/api/v1/places_search/',
-//   type: 'POST',
-//   data: JSON.stringify({}),
-//   contentType:"application/json; charset=utf-8",
-//   success: function (data) {
-//     for (let i = 0; i < data.length; i++) {
-//       // Adding div with class='title_box'
-//       let html_code1 = "<div class='title_box'><h2>" + data[i].name +"</h2> <div class='price_by_night'>$" + data[i].price_by_night +"</div></div>";
-//       // Adding div with class='information'
-//       cond_guests = data[i].max_guest !== 1 ? 's' : '';
-//       let html_code2 = html_code1 + "<div class='information'><div class='max_guest'>" + data[i].max_guest + " Guest" + cond_guests + "</div>";
-//       cond_bedrooms = data[i].number_rooms !== 1 ? 's' : '';
-//       html_code2 += "<div class='number_rooms'>" + data[i].number_rooms + " Bedroom"+ cond_bedrooms +"</div>";
-//       cond_bathrooms = data[i].number_bathrooms !== 1 ? 's' : '';
-//       html_code2 += "<div class='number_bathrooms'>" + data[i].number_bathrooms + " Bathroom"+ cond_bathrooms +"</div></div>";
-//       // Adding div with class='user'
-//       let htmlCode3 = html_code2 + "<div class='description'>" + data[i].description + "</div>"
-//       $('SECTION.places').append('<article>' + htmlCode3 + '</article>');
-//       // clean the variables for each iteration
-//       html_code1 = '';
-//       html_code2 = '';
-//     }
-//   }
-// });
-
-alert('hola')
+console.log("holaaaaaaaa")
 $(document).ready(function(){
   $("#boton").click(function(){
     if (!ids_amenities_copy.length) {
+      console.log("iffff ids_amenities_copy: " + ids_amenities_copy);
       searchData = {}
-      alert(searchData.ameneties)
+      alert(searchData.amenities)
     }
     else {
-      searchData = {'ameneties':ids_amenities_copy}
-      alert(searchData.ameneties)
+      console.log("elseeeeee")
+      searchData = {'amenities':ids_amenities_copy};
+      console.log("searchData " + typeof searchData.amenities)
     }
     $.ajax({
       url: 'http://0.0.0.0:5001/api/v1/places_search/',
@@ -96,7 +70,3 @@ $(document).ready(function(){
     });
   });
 });
-// $('#button').click(function () {
-//   alert('&&&&&&&&&&&&&&&&&&&&&&&&&&&6')
-//   console.log(ids_amenities)
-// })
